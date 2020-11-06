6d8fa6b11c350cc1fe204850a5fc7da7ffe7e4d8
MikeDMorgan <michael.morgan@cruk.cam.ac.uk>
2020-11-06 15:21:58 +0000

    add droplet meta-data file

--- inst/scripts/1.0/make-SMARTseq-metadata.R
+++ inst/scripts/1.0/make-SMARTseq-metadata.R
@@ -4,16 +4,14 @@ info <- data.frame(
                       "rowData",
                       sprintf("colData (day %i)", c(1:5)),
                       sprintf("size factors (day %i)", c(1:5)),
-                      sprintf("reduced dimensions (day %i)", c(1:5)),
-                      sprintf("raw counts (day %i)", c(1:5)))
+                      sprintf("reduced dimensions (day %i)", c(1:5)))
     ),
-    Description = sprintf("%s for the mouse ageing thymus single-cell RNA-seq dataset",
+    Description = sprintf("%s for the mouse ageing thymus SMART-seq2 single-cell RNA-seq dataset",
                           c(sprintf("Processed counts for day %i", c(1:5)),
                             "Per-gene metadata for all days",
                             sprintf("Per-cell metadata for day %i", c(1:5)),
                             sprintf("Size factors for day %i", c(1:5)),
-                            sprintf("Reduced dimensions for day %i", c(1:5)),
-                            sprintf("Raw counts for day %i", c(1:5)))
+                            sprintf("Reduced dimensions for day %i", c(1:5)))
     ),
     RDataPath = c(
         file.path("MouseThymusAgeing", "SMARTseq", "1.0.0",
@@ -21,8 +19,7 @@ info <- data.frame(
                     "rowdata.rds",
                     sprintf("coldata-day%i.rds", c(1:5)),
                     sprintf("sizefactors-day%i.rds", c(1:5)),
-                    sprintf("reduced-dims-day%i.rds", c(1:5)),
-                    sprintf("counts-raw-day%i.rds", c(1:5)))
+                    sprintf("reduced-dims-day%i.rds", c(1:5)))
     )),
     BiocVersion="3.12",
     Genome="mm10",
@@ -30,15 +27,14 @@ info <- data.frame(
     SourceUrl=c(
         rep(
             c("https://content.cruk.cam.ac.uk/jmlab/thymus_data"),
-            c(5 * 5 + 1))
+            c(5 * 4 + 1))
     ),
     SourceVersion=paste(
-        c(rep("raw_counts.mtx.gz", 5),
-          "genes.tsv.gz",
-          rep("meta.tab.gz", 5),
-          rep("sizefactors.tab.gz", 5),
-          rep("pcas.rds", 5),
-          sprintf("sample_%i_counts.tsv.gz", c(1:5))
+        c(rep("QC_counts.tsv.gz", 5),
+          "Gene_info.tsv",
+          rep("meta_data.tsv.gz", 5),
+          rep("size_factors.tsv.gz", 5),
+          rep("reduced_dims.tsv.gz", 5)
         ),
         sep=";"
     ),
@@ -52,4 +48,4 @@ info <- data.frame(
     stringsAsFactors = FALSE
 )
 
-write.csv(file="../../inst/extdata/metadata-SMARTseq.csv", info, row.names=FALSE)
+write.csv(file="../../extdata/metadata-SMARTseq.csv", info, row.names=FALSE)
