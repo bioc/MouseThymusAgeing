ec066142afef0e76e7ad58acbba7252dc6b6a31b
MikeDMorgan <michael.morgan@cruk.cam.ac.uk>
2020-11-25 19:14:06 +0000

    add generic fetching function and droplet data getter

---
+++ R/MouseDropletData.R
@@ -0,0 +1,90 @@
+#' Mouse ageing thymus droplet single-cell RNA-sequencing data
+#'
+#' Obtain the processed counts for the mouse ageing thymus droplet scRNA-seq data set.
+#'
+#' @param samples Integer or character vector specifying the samples for which data (processed or raw) should be obtained.
+#' If \code{NULL} (default), data are returned for all (36) samples.
+
+#' @return
+#' A \linkS4class{SingleCellExperiment} is returned containing processed data from selected samples.
+#'
+#' List elements are named after the corresponding sample.
+#'
+#' @details
+#' This function downloads the data for the mouse ageing thymus droplet scRNA-seq
+#' data from Baran-Gale et al. (2020).
+#'
+#' The dataset contains 6 10X Genomics samples.
+#'
+#'
+#' In the data, cell-containing libraries have already been identified in each sample
+#' using the \code{emptyDrops} function from \pkg{DropletUtils}.
+#' The count matrix contains the raw count vectors for the cells called from all samples in this manner.
+#' Size factors were computed using the \code{computeSumFactors} function from \pkg{scran}.
+#' The column metadata for called cells contains:
+#' \describe{
+#' \item{\code{cell}:}{Character, unique cell identifier across all samples.}
+#' \item{\code{barcode}:}{Character, cell barcode from the 10X Genomics experiment.}
+#' \item{\code{sample}:}{Integer, index of the sample from which the cell was taken.}
+#' \item{\code{pool}:}{Integer, index of the embryo pool from which the sample derived. Samples with the same value are technical, not biological, replicates}
+#' \item{\code{stage}:}{Character, stage of the mouse embryo at which the sample was taken.}
+#' \item{\code{sequencing.batch}:}{Integer, sequencing run in which sample was multiplexed.}
+#' \item{\code{theiler}:}{Character, Theiler stage from which the sample was taken; alternative scheme to \code{stage}.}
+#' \item{\code{doub.density}:}{Numeric, output of (a now-outdated run of) \code{scran::doubletCells}, performed on each sample separately.}
+#' \item{\code{doublet}:}{Logical, whether a cell was called as a doublet.}
+#' \item{\code{cluster}:}{Integer, top-level cluster to which cell was assigned across all samples.}
+#' \item{\code{cluster.sub}:}{Integer, cluster to which cell was assigned when clustered within each \code{cluster}.}
+#' \item{\code{cluster.stage}:}{Integer, top-level cluster to which cell was assigned within individual timepoints.}
+#' \item{\code{cluster.theiler}:}{Integer, top-level cluster to which cell was assigned within individual Theiler stages.}
+#' \item{\code{stripped}:}{Logical, whether a cell was called as a cytoplasm-stripped nucleus.}
+#' \item{\code{celltype}:}{Character, cell type to which the cell was assigned.}
+#' \item{\code{colour}:}{Integer, cell type colour (hex) as in Pijuan-Sala et al. (2019).}
+#' \item{\code{umapX}:}{Numeric, x-coordinate of UMAP plot in Pijuan-Sala et al. (2019).}
+#' \item{\code{umapY}:}{Numeric, y-coordinate of UMAP plot in Pijuan-Sala et al. (2019).}
+#' }
+#' Reduced dimension representations of the data are also available in the \code{reducedDims} slot of the SingleCellExperiment object.
+#' If spliced counts were requested, these will be in the assays slot of the SingleCellExperiment object.
+#' Spliced count matrices were collated using \emph{velocyto} version 0.17.17.
+#' Spliced count matrices will not have had swapped molecules removed, as \emph{velocyto} and \code{DropletUtils::swappedDrops} are not compatible.
+#' However, these should still be effective for calculating RNA velocity estimates using various different tools.
+#'
+#' The raw data contains the unfiltered count matrix for each sample, as generated directly from the CellRanger software.
+#' Swapped molecules have been removed using \code{DropletUtils::swappedDrops}.
+#' No filtering has been performed to identify cells.
+#' This may be useful if performing analyses that need to account for the ambient RNA pool.
+#'
+#' For both raw and processed data, the row metadata contains the Ensembl ID and MGI symbol for each gene.
+#'
+#' @author Aaron Lun, with modification by Jonathan Griffiths
+#' @examples
+#' atlas.data <- EmbryoAtlasData(samples = 1)
+#'
+#' atlas.data <- EmbryoAtlasData(type="processed", samples = 1)
+#'
+#' @references
+#' Pijuan-Sala B, Griffiths JA, Guibentif C et al. (2019).
+#' A single-cell molecular map of mouse gastrulation and early organogenesis.
+#' \emph{Nature} 566, 7745:490-495.
+#'
+#' @export
+#' @importFrom ExperimentHub ExperimentHub
+#' @importFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom BiocGenerics sizeFactors
+#' @importClassesFrom S4Vectors DataFrame
+#' @importFrom methods as
+MouseDropletData <- function(samples=NULL){
+    samp.names <- c("ZsG_1Run1", "ZsG_1Run2", "ZsG_2Run1", "ZsG_2Run2", "ZsG_3Run1", "ZsG_3Run2")
+
+    versions <- list(base="1.0.0")
+
+
+    if(any(!samples %in% samp.names)){
+        warning(paste0("Incorrect samples found. Should be in list:", paste(samp.names, collapse=",")))
+    }
+
+    if(length(intersect(samples, samp.names)) > 0){
+        MouseThymusAgeing:::.getProcOrRaw("Droplet", versions, samples)
+    } else{
+        stop(paste0("No matching samples found. Must be in list:", paste(samp.names, collapse=",")))
+    }
+}
---
+++ R/getProcOrRaw.R
@@ -0,0 +1,67 @@
+#' @importFrom ExperimentHub ExperimentHub
+#' @importFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom BiocGenerics sizeFactors
+#' @importFrom BiocGenerics sizeFactors<-
+#' @importClassesFrom S4Vectors DataFrame
+#' @importFrom S4Vectors List
+#' @importFrom methods as
+#' @importFrom SummarizedExperiment rowData
+#' @importFrom SummarizedExperiment colData
+#'
+#' @export
+.getProcOrRaw <- function(dataset, version, samples){
+    hub <- ExperimentHub()
+    host <- file.path("MouseThymusAgeing", dataset)
+
+    #get rowdata
+    ver <- .fetch_version(version, "rowData")
+    rowdata <- hub[hub$rdatapath==file.path(host, ver, "rowdata.rds")][[1]]
+
+    # Temporary function for data extraction
+    EXTRACTOR <- function(target, version_name) {
+        ver <- .fetch_version(version, version_name)
+        lapply(samples, function(i){
+            hub[hub$rdatapath==file.path(host, ver, sprintf("%s-%s.rds", target, i))][[1]]
+        })
+    }
+
+    count_list <- EXTRACTOR("counts-processed")
+    assays <- list(counts=do.call(cbind, count_list))
+
+    coldata_list <- EXTRACTOR("coldata", "colData")
+    sf_list <- EXTRACTOR("sizefac", "sizefactors")
+    reducedDims_list <- EXTRACTOR("reduced-dims", "reducedDims")
+
+    # Handle data with multiple reducedDims
+    reducedDims_names <- names(reducedDims_list[[1]])
+    reducedDims_combined <- lapply(reducedDims_names, function(x){
+        do.call(rbind, lapply(reducedDims_list, function(y) y[[x]]))
+    })
+    names(reducedDims_combined) <- reducedDims_names
+
+    sce <- SingleCellExperiment(
+        assays=assays,
+        colData=as(do.call(rbind, coldata_list), "DataFrame"),
+        reducedDims=reducedDims_combined,
+        rowData=rowdata
+    )
+    sizeFactors(sce) <- unlist(sf_list)
+    rownames(sce) <- rowData(sce)$ENSEMBL
+    colnames(sce) <- colData(sce)$cell
+    return(sce)
+}
+
+#from Aaron Lun's celldex with modification
+#for consistent usage in-package, use "base" as element 1, anything
+#with a different version gets an entry in version in .getProcOrRaw
+#there should be in {counts,spliced,raw_counts,
+#sizefactors,rowData,colData,reducedDims}
+#spliced represents version for all spliced count measurements
+.fetch_version <- function(version, field) {
+    opt <- version[[field]]
+    if (is.null(opt)) {
+        version[[1]]
+    } else {
+        opt
+    }
+}
