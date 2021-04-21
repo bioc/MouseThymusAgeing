2b9cf6a17baf80937aed9aef07542194bf64f94e
MikeDMorgan <michael.morgan@cruk.cam.ac.uk>
2021-04-21 13:35:55 +0100

    remove injected double-tab from sample names in meta-data objects

--- R/DropletMetadata.R
+++ R/DropletMetadata.R
@@ -28,36 +28,36 @@
 DropletMetadata <- read.table(
     text =
         "sample,Gender,HTO,ncells
-        ZsG_1stRun1,female,Wk1_ZsGp,5160
-        ZsG_1stRun1,female,Wk4_ZsGp,3902
-        ZsG_1stRun1,female,Wk16_ZsGp,3798
-        ZsG_1stRun2,female,Wk1_ZsGp,3360
-        ZsG_1stRun2,female,Wk4_ZsGp,2355
-        ZsG_1stRun2,female,Wk16_ZsGp,4199
-        ZsG_2ndRun1,female,Wk1_ZsGn,2090
-        ZsG_2ndRun1,female,Wk1_ZsGp,1927
-        ZsG_2ndRun1,female,Wk4_ZsGn,1870
-        ZsG_2ndRun1,female,Wk4_ZsGp,2759
-        ZsG_2ndRun1,female,Wk16_ZsGn,668
-        ZsG_2ndRun1,female,Wk16_ZsGp,3060
-        ZsG_2ndRun2,female,Wk1_ZsGn,2004
-        ZsG_2ndRun2,female,Wk1_ZsGp,1893
-        ZsG_2ndRun2,female,Wk4_ZsGn,1830
-        ZsG_2ndRun2,female,Wk4_ZsGp,2721
-        ZsG_2ndRun2,female,Wk16_ZsGn,736
-        ZsG_2ndRun2,female,Wk16_ZsGp,2894
-        ZsG_3rdRun1,female,Wk1_ZsGn,1409
-        ZsG_3rdRun1,female,Wk1_ZsGp,1854
-        ZsG_3rdRun1,female,Wk4_ZsGn,1403
-        ZsG_3rdRun1,female,Wk4_ZsGp,2368
-        ZsG_3rdRun1,female,Wk16_ZsGn,1055
-        ZsG_3rdRun1,female,Wk16_ZsGp,1926
-        ZsG_3rdRun2,female,Wk1_ZsGn,1475
-        ZsG_3rdRun2,female,Wk1_ZsGp,2270
-        ZsG_3rdRun2,female,Wk4_ZsGn,1827
-        ZsG_3rdRun2,female,Wk4_ZsGp,3083
-        ZsG_3rdRun2,female,Wk16_ZsGn,1072
-        ZsG_3rdRun2,female,Wk16_ZsGp,2212",
+ZsG_1stRun1,female,Wk1_ZsGp,5160
+ZsG_1stRun1,female,Wk4_ZsGp,3902
+ZsG_1stRun1,female,Wk16_ZsGp,3798
+ZsG_1stRun2,female,Wk1_ZsGp,3360
+ZsG_1stRun2,female,Wk4_ZsGp,2355
+ZsG_1stRun2,female,Wk16_ZsGp,4199
+ZsG_2ndRun1,female,Wk1_ZsGn,2090
+ZsG_2ndRun1,female,Wk1_ZsGp,1927
+ZsG_2ndRun1,female,Wk4_ZsGn,1870
+ZsG_2ndRun1,female,Wk4_ZsGp,2759
+ZsG_2ndRun1,female,Wk16_ZsGn,668
+ZsG_2ndRun1,female,Wk16_ZsGp,3060
+ZsG_2ndRun2,female,Wk1_ZsGn,2004
+ZsG_2ndRun2,female,Wk1_ZsGp,1893
+ZsG_2ndRun2,female,Wk4_ZsGn,1830
+ZsG_2ndRun2,female,Wk4_ZsGp,2721
+ZsG_2ndRun2,female,Wk16_ZsGn,736
+ZsG_2ndRun2,female,Wk16_ZsGp,2894
+ZsG_3rdRun1,female,Wk1_ZsGn,1409
+ZsG_3rdRun1,female,Wk1_ZsGp,1854
+ZsG_3rdRun1,female,Wk4_ZsGn,1403
+ZsG_3rdRun1,female,Wk4_ZsGp,2368
+ZsG_3rdRun1,female,Wk16_ZsGn,1055
+ZsG_3rdRun1,female,Wk16_ZsGp,1926
+ZsG_3rdRun2,female,Wk1_ZsGn,1475
+ZsG_3rdRun2,female,Wk1_ZsGp,2270
+ZsG_3rdRun2,female,Wk4_ZsGn,1827
+ZsG_3rdRun2,female,Wk4_ZsGp,3083
+ZsG_3rdRun2,female,Wk16_ZsGn,1072
+ZsG_3rdRun2,female,Wk16_ZsGp,2212",
     header = TRUE,
     sep = ",",
     stringsAsFactors = FALSE)
--- R/SMARTseqMetadata.R
+++ R/SMARTseqMetadata.R
@@ -27,31 +27,31 @@
 SMARTseqMetadata <- read.table(
     text =
         "sample,Age,Gender,ncells
-        day1,1wk,female,10
-        day1,4wk,female,19
-        day1,16wk,female,10
-        day1,32wk,female,43
-        day1,52wk,female,52
-        day2,1wk,female,153
-        day2,4wk,female,157
-        day2,16wk,female,156
-        day2,32wk,female,87
-        day2,52wk,female,108
-        day3,1wk,female,101
-        day3,4wk,female,111
-        day3,16wk,female,29
-        day3,32wk,female,148
-        day3,52wk,female,77
-        day4,1wk,female,60
-        day4,4wk,female,162
-        day4,16wk,female,49
-        day4,32wk,female,44
-        day4,52wk,female,4
-        day5,1wk,female,109
-        day5,4wk,female,150
-        day5,16wk,female,74
-        day5,32wk,female,174
-        day5,52wk,female,240",
+day1,1wk,female,10
+day1,4wk,female,19
+day1,16wk,female,10
+day1,32wk,female,43
+day1,52wk,female,52
+day2,1wk,female,153
+day2,4wk,female,157
+day2,16wk,female,156
+day2,32wk,female,87
+day2,52wk,female,108
+day3,1wk,female,101
+day3,4wk,female,111
+day3,16wk,female,29
+day3,32wk,female,148
+day3,52wk,female,77
+day4,1wk,female,60
+day4,4wk,female,162
+day4,16wk,female,49
+day4,32wk,female,44
+day4,52wk,female,4
+day5,1wk,female,109
+day5,4wk,female,150
+day5,16wk,female,74
+day5,32wk,female,174
+day5,52wk,female,240",
     header = TRUE,
     sep = ",",
     stringsAsFactors = FALSE)
